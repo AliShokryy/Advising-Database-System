cc562994c6e827d23d69d44b2614d6b0f3b535fd
Abdullah Mahmoud <abdullahmahmoud289@gmail.com>
2023-12-14 01:31:45 +0200

    Finished views of Students and Advisors

--- Team6_Advising/Team6_Advising/Pages/Students/FirstMakeUp.cshtml.cs
+++ Team6_Advising/Team6_Advising/Pages/Students/FirstMakeUp.cshtml.cs
@@ -7,8 +7,41 @@ namespace Team6_Advising.Pages.Students
 {
     public class FirstMakeUpModel : PageModel
     {
-        public void OnGet()
+        public List<StudentExam> studentExams = new List<StudentExam>();
+        public void OnGet(String? id)
         {
+            try
+            {
+                int student_id = int.Parse(id);
+                string connectionString = Environment.GetEnvironmentVariable("ConnectionString");
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string commandText = "SELECT Es.* FROM Exam_Student Es INNER JOIN MakeUp_Exam Me ON Es.exam_id = Me.exam_id WHERE student_id =" + student_id + "AND Me.type = 'First_makeup'";
+                    using (SqlCommand command = new SqlCommand(commandText, connection))
+                    {                        
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                StudentExam studentExam = new StudentExam();
+                                studentExam.exam_id = reader.GetInt32(0);
+                                studentExam.student_id = reader.GetInt32(1);
+                                studentExam.course_id = reader.GetInt32(2);
+                                studentExams.Add(studentExam);
+                            }
+
+                        }
+                        
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                ViewData["Message"] = "error";
+                Console.WriteLine(e.ToString());
+            }
         }
         public void OnPost(String? id)
         {
@@ -30,7 +63,8 @@ namespace Team6_Advising.Pages.Students
                         command.Parameters.Add(new SqlParameter("@studentCurr_sem", current_sem));
                         command.ExecuteNonQuery();
                     }
-
+                    connection.Close();
+                    OnGet(id);
                 }
 
             }
@@ -41,6 +75,13 @@ namespace Team6_Advising.Pages.Students
             }
 
 
+        }
+
+        public class StudentExam
+        {
+            public int exam_id;
+            public int student_id;
+            public int course_id;
         }
     }
 }
--- Team6_Advising/Team6_Advising/Pages/Students/SecondMakeUp.cshtml.cs
+++ Team6_Advising/Team6_Advising/Pages/Students/SecondMakeUp.cshtml.cs
@@ -2,13 +2,47 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Data;
 using System.Data.SqlClient;
+using static Team6_Advising.Pages.Students.FirstMakeUpModel;
 
 namespace Team6_Advising.Pages.Students
 {
     public class SecondMakeUpModel : PageModel
     {
-        public void OnGet()
+        public List<StudentExam> studentExams = new List<StudentExam>();
+        public void OnGet(String? id)
         {
+            try
+            {
+                int student_id = int.Parse(id);
+                string connectionString = Environment.GetEnvironmentVariable("ConnectionString");
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string commandText = "SELECT Es.* FROM Exam_Student Es INNER JOIN MakeUp_Exam Me ON Es.exam_id = Me.exam_id WHERE student_id =" + student_id + "AND Me.type = 'Second_makeup'";
+                    using (SqlCommand command = new SqlCommand(commandText, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                StudentExam studentExam = new StudentExam();
+                                studentExam.exam_id = reader.GetInt32(0);
+                                studentExam.student_id = reader.GetInt32(1);
+                                studentExam.course_id = reader.GetInt32(2);
+                                studentExams.Add(studentExam);
+                            }
+
+                        }
+
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                ViewData["Message"] = "error";
+                Console.WriteLine(e.ToString());
+            }
         }
         public void OnPost(String? id)
         {
@@ -30,6 +64,8 @@ namespace Team6_Advising.Pages.Students
                         command.Parameters.Add(new SqlParameter("@studentCurr_sem", current_sem));
                         command.ExecuteNonQuery();
                     }
+                    connection.Close();
+                    OnGet(id);
 
                 }
 
@@ -40,6 +76,12 @@ namespace Team6_Advising.Pages.Students
                 Console.WriteLine(e.ToString());
             }
 
+        }
+        public class StudentExam
+        {
+            public int exam_id;
+            public int student_id;
+            public int course_id;
         }
     }
 }
