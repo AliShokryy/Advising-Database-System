d42056e64cfb22670a9f4bfe21f58dd13120eb66
Ahmed Hawater <hawatercoding@gmail.com>
2024-01-28 21:10:04 +0200

    Prepare the repo to be public
    removed unecessary files and folders and unpacked nested folders to
    simplify the repo representaions as possible

---
+++ Team6_Advising/Pages/Admin/Login.cshtml.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Team6_Advising.Pages.Admin
+{
+    public class LoginModel : PageModel
+    {
+        const int ADMIN_ID = 1;
+        const string PASSWORD = "admin";
+        
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+
+            try
+            {
+                int adminId = int.Parse(Request.Form["id"]);
+                string password = Request.Form["password"];
+                if (adminId == ADMIN_ID && password== PASSWORD)
+                {
+                    Console.WriteLine("Login Successful");
+                    Response.Redirect("/Admin/Index?id=1");
+
+                }
+                else
+                {
+                    Console.WriteLine("Login failed");
+                    ViewData["Message"] = "Login Failed";
+
+                }
+            }
+            catch (Exception e)
+            {
+                ViewData["Message"] = "Admin not found";
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
